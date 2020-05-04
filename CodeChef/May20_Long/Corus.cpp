3d634bef65e7154dced8aeaf909bc7215bbebbdc
sahuankit010 <32036503+sahuankit010@users.noreply.github.com>
2020-05-04 16:45:48 +0530

    Second Question Submission

---
+++ CodeChef/May20_Long/Corus.cpp
@@ -0,0 +1,66 @@
+/*******************
+  Ankit Sahu
+  SRMIST Chennai
+ ***********************/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define fore(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
+#define ll long long
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef long long i64;
+typedef vector<i64> vi64;
+typedef vector<vi64> vvi64;
+typedef pair<i64, i64> pi64;
+typedef double ld;
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(10);
+    cout << fixed;
+    int t;
+    cin>>t;
+    while(t--){
+    	int n,q;
+    	cin>>n>>q;
+    	string str;
+    	cin>>str;
+    	unordered_map<char,int> umap;
+    	for(int i=0;i<str.length();i++){
+    	    umap[str[i]]++;
+    	}
+    	while(q--){
+    	    ll c;
+    	    cin>>c;
+    	    int ans=0;
+    	    for(auto it = umap.begin(); it!= umap.end(); it++){
+    	        if(it->second>c){
+    	            ans=ans+(it->second-c);
+    	        }
+    	    }
+    	    cout<<ans<<endl;
+    	}
+	}
+
+
+	return 0;
+}
+
